cb9a591c5b049780e5c091ade82b7a2bc09a1744
KateZzh <ekaterin.pro2@gmail.com>
2023-06-12 23:25:39 +0300

    app.js changed

--- app.js
+++ app.js
@@ -1,15 +1,43 @@
-document.querySelector(".dropdown-list-el").addEventListener("click", () => {
-  const dropdownListText = document.querySelector(".dropdown-list-text");
+const dropdownListEl = document.querySelectorAll(".dropdown-list-el");
+const dropdownListText = document.querySelectorAll(".dropdown-list-text");
+const dropdownListImg = document.querySelectorAll(".dropdown-list-img");
 
-  if (dropdownListText.hidden == true) {
-    dropdownListText.hidden = false;
-    document.querySelector(
-      ".dropdown-list-img"
-    ).style = `background-image: url(./assets/minus.svg);`;
+dropdownListEl[0].addEventListener("click", function () {
+  if (dropdownListText[0].hidden == true) {
+    dropdownListText[0].hidden = false;
+    dropdownListImg[0].style = `background-image: url(./assets/minus.svg);`;
   } else {
-    dropdownListText.hidden = true;
-    document.querySelector(
-      ".dropdown-list-img"
-    ).style = `background-image: url(./assets/plus.svg)`;
+    dropdownListText[0].hidden = true;
+    dropdownListImg[0].style = `background-image: url(./assets/plus.svg)`;
+  }
+});
+
+dropdownListEl[1].addEventListener("click", function () {
+  if (dropdownListText[1].hidden == true) {
+    dropdownListText[1].hidden = false;
+    dropdownListImg[1].style = `background-image: url(./assets/minus.svg);`;
+  } else {
+    dropdownListText[1].hidden = true;
+    dropdownListImg[1].style = `background-image: url(./assets/plus.svg)`;
+  }
+});
+
+dropdownListEl[2].addEventListener("click", function () {
+  if (dropdownListText[2].hidden == true) {
+    dropdownListText[2].hidden = false;
+    dropdownListImg[2].style = `background-image: url(./assets/minus.svg);`;
+  } else {
+    dropdownListText[2].hidden = true;
+    dropdownListImg[2].style = `background-image: url(./assets/plus.svg)`;
+  }
+});
+
+dropdownListEl[3].addEventListener("click", function () {
+  if (dropdownListText[3].hidden == true) {
+    dropdownListText[3].hidden = false;
+    dropdownListImg[3].style = `background-image: url(./assets/minus.svg);`;
+  } else {
+    dropdownListText[3].hidden = true;
+    dropdownListImg[3].style = `background-image: url(./assets/plus.svg)`;
   }
 });
